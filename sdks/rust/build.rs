03fc6ee2935c0d27b31281734157ba1cd52e6314
Waridley <Waridley64@gmail.com>
2021-05-05 12:27:51 -0500

    Initial pass at rewriting Rust SDK using Tonic

---
+++ sdks/rust/build.rs
@@ -0,0 +1,84 @@
+use std::{path::{PathBuf}, env::var};
+use std::fs::File;
+use std::io::{BufReader, BufRead, Write};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+	println!("cargo:rerun-if-changed=../../proto");
+	println!("cargo:rerun-if-changed=build.rs");
+	println!("cargo:rerun-if-changed={}", var("OUT_DIR").unwrap());
+	
+	let mut builder = tonic_build::configure();
+	
+	builder = match var("CARGO_FEATURE_CLIENT") {
+		Ok(s) if s == "1" => builder.build_client(true),
+		_ => { builder.build_client(false) }
+	};
+	
+	builder = match var("CARGO_FEATURE_SERVER") {
+		Ok(s) if s == "1" => builder.build_server(true),
+		_ => { builder.build_server(false) }
+	};
+	
+	let include_path = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap())
+		.parent().unwrap() // sdks
+		.parent().unwrap() // agones
+		.join("proto");
+	
+	let protos = vec![
+		var("CARGO_FEATURE_ALLOCATION").ok()
+			.and_then(|s| (s == "1").then(|| include_path.join("allocation/allocation.proto"))),
+		var("CARGO_FEATURE_SDK").ok()
+			.and_then(|s| (s == "1").then(|| include_path.join("sdk/sdk.proto"))),
+		var("CARGO_FEATURE_ALPHA").ok()
+			.and_then(|s| (s == "1").then(|| include_path.join("sdk/alpha/alpha.proto"))),
+		var("CARGO_FEATURE_BETA").ok()
+			.and_then(|s| (s == "1").then(|| include_path.join("sdk/beta/beta.proto"))),
+	].into_iter()
+		.filter(|opt| opt.is_some())
+		.map(|opt| opt.unwrap())
+		.collect::<Vec<_>>();
+	
+	dbg!(builder.compile(
+		&*protos,
+		&[
+			include_path.join("googleapis"),
+			include_path,
+		],
+	))?;
+	
+	//TODO: There's probably a better way to parse the proto files to find the includes
+	let rs_incl_dir = PathBuf::from(var("OUT_DIR").unwrap()).join("includes");
+	std::fs::create_dir_all(&rs_incl_dir);
+	for path in protos {
+		File::open(&path)
+			.and_then(|proto_file| {
+				let reader = BufReader::new(proto_file);
+				let mut includes = vec![];
+				for line in reader.lines() {
+					if let Ok(line) = line {
+						if line.starts_with("message ") {
+							let line = line.trim_start_matches("message ");
+							let non_alpha = line.find(|c: char| !c.is_ascii_alphabetic()).unwrap();
+							includes.push(line[..non_alpha].trim().to_string());
+						}
+					}
+				}
+				File::create(rs_incl_dir
+					.join(&*path
+						.file_name().unwrap()
+						.to_str().unwrap()
+						.replace(".proto", ".rs")
+					)
+				)
+					.and_then(|mut out_file| {
+						out_file.write(b"pub use crate::proto::{")?;
+						out_file.write(includes.join(",").as_bytes())?;
+						out_file.write(b"};")?;
+						Ok(())
+					})
+			})?;
+	}
+	
+	Ok(())
+}
--- sdks/rust/src/lib.rs
+++ sdks/rust/src/lib.rs
@@ -12,14 +12,131 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! the Rust game server SDK
-#[macro_use]
-extern crate error_chain;
+pub mod proto {
+	#[cfg(feature = "sdk")]
+	pub mod sdk {
+		tonic::include_proto!("agones.dev.sdk");
+	}
+	#[cfg(feature = "alpha")]
+	pub mod alpha {
+		tonic::include_proto!("agones.dev.sdk.alpha");
+	}
+	#[cfg(feature = "beta")]
+	pub mod beta {
+		tonic::include_proto!("agones.dev.sdk.beta");
+	}
+	#[cfg(feature = "allocation")]
+	pub mod allocation {
+		tonic::include_proto!("allocation");
+	}
+}
 
-pub mod alpha;
-pub mod errors;
-mod grpc;
-mod sdk;
-pub mod types;
-
-pub use sdk::Sdk;
+pub mod sdk {
+	#[cfg(feature = "sdk")]
+	pub use crate::proto::sdk::{
+		Empty, KeyValue, Duration,
+	};
+	#[cfg(feature = "sdk")]
+	use crate::proto::sdk::sdk_client::SdkClient;
+	#[cfg(feature = "alpha")]
+	use crate::proto::alpha;
+	
+	use tonic::{
+		Response, Status, IntoRequest,
+		codegen::StdError,
+		transport::{Channel, Endpoint, Error,}
+	};
+	
+	pub struct Sdk {
+		#[cfg(feature = "sdk")]
+		stable: SdkClient<Channel>,
+		#[cfg(feature = "alpha")]
+		alpha: alpha::sdk_client::SdkClient<Channel>,
+	}
+	
+	impl Sdk {
+		pub fn new<D>(dst: D) -> Result<Self, tonic::transport::Error>
+		where
+			D: std::convert::TryInto<tonic::transport::Endpoint>,
+			D::Error: Into<StdError>,
+		{
+			let conn = Endpoint::new(dst)?.connect_lazy()?;
+			Ok(Sdk {
+				#[cfg(feature = "sdk")]
+				stable: SdkClient::new(conn.clone()),
+				#[cfg(feature = "alpha")]
+				alpha: alpha::sdk_client::SdkClient::new(conn),
+			})
+		}
+	}
+	
+	#[cfg(feature = "sdk")]
+	impl Sdk {
+		/// Call when the GameServer is ready
+		pub async fn ready(
+			&mut self,
+			request: impl IntoRequest<Empty>,
+		) -> Result<Response<Empty>, Status> {
+			self.stable.ready(request).await
+		}
+		
+		pub async fn shutdown(
+			&mut self,
+			request: impl IntoRequest<Empty>,
+		) -> Result<Response<Empty>, Status> {
+			self.stable.shutdown(request).await
+		}
+		
+		pub async fn set_label(
+			&mut self,
+			request: impl tonic::IntoRequest<KeyValue>,
+		) -> Result<Response<Empty>, Status> {
+			self.stable.set_label(request).await
+		}
+		
+		pub async fn set_annotation(
+			&mut self,
+			request: impl tonic::IntoRequest<KeyValue>,
+		) -> Result<Response<Empty>, Status> {
+			self.stable.set_annotation(request).await
+		}
+		
+		pub async fn allocate(
+			&mut self,
+			request: impl tonic::IntoRequest<Empty>,
+		) -> Result<Response<Empty>, Status> {
+			self.stable.allocate(request).await
+		}
+		
+		pub async fn reserve(
+			&mut self,
+			request: impl tonic::IntoRequest<Duration>,
+		) -> Result<Response<Empty>, Status> {
+			self.stable.reserve(request).await
+		}
+	}
+	
+	#[cfg(feature = "alpha")]
+	impl Sdk {
+		pub async fn set_player_capacity(
+			&mut self,
+			request: impl tonic::IntoRequest<alpha::Count>,
+		) -> Result<Response<alpha::Empty>, Status> {
+			self.alpha.set_player_capacity(request).await
+		}
+		
+		pub async fn player_connect(
+			&mut self,
+			request: impl tonic::IntoRequest<alpha::PlayerId>,
+		) -> Result<Response<alpha::Bool>, Status> {
+			self.alpha.player_connect(request).await
+		}
+		
+		pub async fn player_disconnect(
+			&mut self,
+			request: impl tonic::IntoRequest<alpha::PlayerId>,
+		) -> Result<Response<alpha::Bool>, Status> {
+			self.alpha.player_disconnect(request).await
+		}
+	}
+}
