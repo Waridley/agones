74e246a3dda65f25653038a918a55bfa611f9437
Waridley <Waridley64@gmail.com>
2021-05-08 23:19:15 -0500

    Remove Ipv6 support because the SDK sidecar can't bind to it apparently

--- sdks/rust/src/lib.rs
+++ sdks/rust/src/lib.rs
@@ -47,7 +47,7 @@ pub mod sdk {
 		codegen::{StdError, HttpBody, },
 		transport::{Channel, Endpoint,}
 	};
-	use std::net::{Ipv6Addr, Ipv4Addr, SocketAddrV6, SocketAddrV4};
+	use std::net::{Ipv4Addr, SocketAddrV4};
 	use tonic::codegen::http::uri::{Builder as UriBuilder, Scheme};
 	use tokio_stream::StreamExt;
 	use std::ops::{Deref, DerefMut};
@@ -103,7 +103,7 @@ pub mod sdk {
 			Ok(Self::new(chan))
 		}
 		
-		pub async fn new_v4(port: u16) -> Result<Self, tonic::transport::Error> {
+		pub async fn with_port(port: u16) -> Result<Self, tonic::transport::Error> {
 			Self::connect(UriBuilder::new()
 				.scheme(Scheme::HTTP)
 				.authority(
@@ -115,24 +115,8 @@ pub mod sdk {
 			).await
 		}
 		
-		pub async fn new_v6(port: u16) -> Result<Self, tonic::transport::Error> {
-			Self::connect(UriBuilder::new()
-				.scheme(Scheme::HTTP)
-				.authority(
-					&*SocketAddrV6::new(Ipv6Addr::LOCALHOST, port, 0, 0).to_string()
-				)
-				.path_and_query("")
-				.build()
-				.unwrap()
-			).await
-		}
-		
-		pub async fn default_v4() -> Result<Self, tonic::transport::Error> {
-			Self::new_v4(9357).await
-		}
-		
-		pub async fn default_v6() -> Result<Self, tonic::transport::Error> {
-			Self::new_v6(9357).await
+		pub async fn default() -> Result<Self, tonic::transport::Error> {
+			Self::with_port(9357).await
 		}
 		
 		pub async fn health(&mut self) -> Result<Response<Empty>, Status> {
